a6902ce3d09f98b16abe8cc79f70246226ce254a
prakhardoneria <gamesprotec@gmail.com>
2025-03-23 15:33:24 +0530

    Login status check

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'firebase_options.dart';
 import 'admin_login.dart';
+import 'admin_dashboard.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -14,7 +16,25 @@ class AdminApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: AdminLoginScreen(),
+      home: AuthCheck(), // Check authentication state
+    );
+  }
+}
+
+class AuthCheck extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Scaffold(body: Center(child: CircularProgressIndicator()));
+        } else if (snapshot.hasData && snapshot.data != null) {
+          return AdminDashboard(); // If logged in, go to dashboard
+        } else {
+          return AdminLoginScreen(); // If not logged in, go to login
+        }
+      },
     );
   }
 }
